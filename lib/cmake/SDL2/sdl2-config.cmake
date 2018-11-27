8dceac35b19be1feb5f30d1100cdfff13f5f2d03
RednaD <russo.alice.42@gmail.com>
2018-11-27 17:50:56 +0100

    Maybe fixed SDL2 ?

--- lib/cmake/SDL2/sdl2-config.cmake
+++ lib/cmake/SDL2/sdl2-config.cmake
@@ -1,10 +1,10 @@
 # sdl2 cmake project-config input for ./configure scripts
 
-set(prefix "/Users/arusso/.brew/Cellar/sdl2/2.0.8") 
+set(prefix "/Users/*/.brew/Cellar/sdl2/2.0.8") 
 set(exec_prefix "${prefix}")
 set(libdir "${exec_prefix}/lib")
-set(SDL2_PREFIX "/Users/arusso/.brew/Cellar/sdl2/2.0.8")
-set(SDL2_EXEC_PREFIX "/Users/arusso/.brew/Cellar/sdl2/2.0.8")
+set(SDL2_PREFIX "/Users/*/.brew/Cellar/sdl2/2.0.8")
+set(SDL2_EXEC_PREFIX "/Users/*/.brew/Cellar/sdl2/2.0.8")
 set(SDL2_LIBDIR "${exec_prefix}/lib")
 set(SDL2_INCLUDE_DIRS "${prefix}/include/SDL2")
 set(SDL2_LIBRARIES "-L${SDL2_LIBDIR}  -lSDL2 ")
