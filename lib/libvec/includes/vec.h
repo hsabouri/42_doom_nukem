38356c652ccf88b38f2182e6c6552158a224b1fd
Hugo Sabourin <hsabouri@student.42.fr>
2018-12-26 11:10:00 +0100

    include order

--- lib/libvec/includes/vec.h
+++ lib/libvec/includes/vec.h
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   vec.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsabouri <hsabouri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hugo <hugo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/29 14:16:10 by hugo              #+#    #+#             */
-/*   Updated: 2018/12/21 17:49:25 by hsabouri         ###   ########.fr       */
+/*   Updated: 2018/12/26 11:09:10 by hugo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VEC_H
 # define VEC_H
 
-# include <float_math.h>
-# include <float_vec.h>
 # include <fixed_math.h>
 # include <fixed_vec.h>
+# include <float_math.h>
+# include <float_vec.h>
 
 #endif
