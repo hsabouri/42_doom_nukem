fe17ebc5b6fa4e7f4020e4ae9131e947a1c4d751
Hugo SABOURIN <hsabouri@student.42.fr>
2019-01-05 13:35:57 +0100

    removed unsused variables in mouse

--- srcs/move.c
+++ srcs/move.c
@@ -6,7 +6,7 @@
 /*   By: hsabouri <hsabouri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/05 11:42:34 by hsabouri          #+#    #+#             */
-/*   Updated: 2019/01/05 12:13:52 by hsabouri         ###   ########.fr       */
+/*   Updated: 2019/01/05 13:34:51 by hsabouri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 t_ph			update_mouse(t_event *events, t_ph player_physic)
 {
-	const float		pi = 3.141592;
-	const float		eps = 0.001;
-
 	player_physic.look.u += LOOK_SPEED * (events->x - events->old_x);
 	player_physic.look.v += LOOK_SPEED * (events->y - events->old_y);
-	printf("%d - %d\n", events->x, events->y);
 	events->old_x = events->x;
 	events->old_y = events->y;
 	return (player_physic);
