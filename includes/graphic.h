dc9eaceaa086bb34773a85b5197b026ed75d341f
Hugo SABOURIN <hsabouri@student.42.fr>
2019-01-21 12:47:48 +0100

    Resolve bug

--- includes/graphic.h
+++ includes/graphic.h
@@ -6,7 +6,7 @@
 /*   By: hsabouri <hsabouri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 17:27:41 by hsabouri          #+#    #+#             */
-/*   Updated: 2019/01/20 18:15:44 by hsabouri         ###   ########.fr       */
+/*   Updated: 2019/01/21 12:29:55 by hsabouri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 # define HEIGHT 480
 # define PWIDTH	2.5
 # define RATIO WIDTH / PWIDTH
-# define PDIS	1.0
+# define PDIS 1.0
 
 # define WHITE		(t_color){255,255,255,255}
 # define RED		(t_color){255,0,0,255}
@@ -65,30 +65,30 @@ typedef struct	s_bres
 	int			inc[2];
 }				t_bres;
 
-typedef struct		s_ray
+typedef struct	s_ray
 {
 	t_fvec2		dir;
 	int			id;
 	ssize_t		mask_wall;
-}					t_ray;
+}				t_ray;
 
-typedef struct		s_i_wall
+typedef struct	s_i_wall
 {
 	size_t		wall_id;
 	t_fvec2		a;
 	t_fvec2		b;
-}					t_i_wall;
+}				t_i_wall;
 
-typedef struct		s_hit
+typedef struct	s_hit
 {
 	ssize_t		wall;
 	t_fixed		t;
 	t_fixed		u;
-}					t_hit;
+}				t_hit;
 
 # define Y_PRECISION 8
 
-typedef struct		s_proj
+typedef struct	s_proj
 {
 	t_wall	wall;
 	int		top;
@@ -101,24 +101,23 @@ typedef struct		s_proj
 	t_fixed	y_iter;
 	t_fixed	x_col;
 	t_color	ambient;
-}					t_proj;
+}				t_proj;
 
-typedef struct		s_last
+typedef struct	s_last
 {
 	size_t	frame;
 	int		x;
 	int		start;
 	int		end;
-}					t_last;
+}				t_last;
 
-void				background(t_color *buf, t_color color);
-void				bresenham(t_color *buff, t_pix a, t_pix b, t_color color);
-void				draw_point(t_fvec2 point, int s, t_color *buf, t_color c);
+void			background(t_color *buf, t_color color);
+void			bresenham(t_color *buff, t_pix a, t_pix b, t_color color);
+void			draw_point(t_fvec2 point, int s, t_color *buf, t_color c);
 
-t_vec2				player_space(t_vec2 vec, t_ph physic);
-void				raycast(t_game game, size_t sector_id, t_color *buf);
-void				render_wall(int x, t_proj proj, t_color *buf,\
-					size_t frame);
-t_color				get_wall_pixel(t_mat mat, t_proj proj, int y);
+t_vec2			player_space(t_vec2 vec, t_ph physic);
+void			raycast(t_game game, size_t sector_id, t_color *buf);
+void			render_wall(int x, t_proj proj, t_color *buf, size_t frame);
+t_color			get_wall_pixel(t_mat mat, t_proj proj, int y);
 
 #endif
