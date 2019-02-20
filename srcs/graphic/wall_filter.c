1bee9fb7a2b959f20b80aa20d5775345f920b5b6
Hugo SABOURIN <hsabouri@student.42.fr>
2019-02-20 12:02:02 +0100

    Filter is fully working

--- srcs/graphic/wall_filter.c
+++ srcs/graphic/wall_filter.c
@@ -6,7 +6,7 @@
 /*   By: hsabouri <hsabouri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 18:29:26 by hsabouri          #+#    #+#             */
-/*   Updated: 2019/02/19 19:07:35 by hsabouri         ###   ########.fr       */
+/*   Updated: 2019/02/20 12:01:03 by hsabouri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,8 @@ void			build_rays(t_vec2 rays[2], t_ph physic, t_vec2 a, t_vec2 b)
 		rays[1] = v_rays[1];
 }
 
-t_bunch			filter(t_game game, t_sector sector, t_vec2 r[2], t_vec2 pos, ssize_t mask)
+t_bunch			filter(t_game game, t_sector sector, t_vec2 r[2], t_vec2 pos,\
+ssize_t mask)
 {
 	t_bunch		res;
 	t_i_wall	current;
@@ -47,19 +48,20 @@ t_bunch			filter(t_game game, t_sector sector, t_vec2 r[2], t_vec2 pos, ssize_t 
 	res.sector = sector;
 	current.wall_id = sector.start;
 	res.nwalls = 0;
-	printf("Retained walls of sector %zu:\n", sector.sector_id);
+	//printf("Retained walls of sector %zu:\n", sector.sector_id);
 	while (current.wall_id < sector.start + sector.number)
 	{
 		wall = game.walls[current.wall_id];
 		a = vec2_sub(game.points[wall.a], pos);
 		b = vec2_sub(game.points[wall.b], pos);
-		if ((size_t)mask != current.wall_id && ((vec2_cross(r[0], a).z <= 0 && vec2_cross(r[1], a).z >= 0) ||
-			(vec2_cross(r[0], b).z <= 0 && vec2_cross(r[1], b).z >= 0)))
+		if ((size_t)mask != current.wall_id &&
+		!((vec2_cross(r[0], a).z >= 0 && vec2_cross(r[0], b).z >= 0) ||
+		(vec2_cross(r[1], a).z <= 0 && vec2_cross(r[1], b).z <= 0)))
 		{
 			current.a = vec2_to_fvec2(game.points[wall.a]);
 			current.b = vec2_to_fvec2(game.points[wall.b]);
 			res.walls[res.nwalls] = current;
-			printf("	%zu\n", current.wall_id);
+			//printf("	%zu\n", current.wall_id);
 			++res.nwalls;
 		}
 		++current.wall_id;
