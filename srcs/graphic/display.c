bbfd8c720dd73670e59be77949f429876610c1ed
Hugo SABOURIN <hsabouri@student.42.fr>
2019-03-02 15:25:09 +0100

    textures on portal

--- srcs/graphic/display.c
+++ srcs/graphic/display.c
@@ -6,18 +6,47 @@
 /*   By: hsabouri <hsabouri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/02 14:16:52 by hsabouri          #+#    #+#             */
-/*   Updated: 2019/03/02 14:17:27 by hsabouri         ###   ########.fr       */
+/*   Updated: 2019/03/02 15:23:42 by hsabouri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <graphic.h>
 
-void	draw_wall(int id, t_proj proj, t_color *buf, u_int32_t *ids)
+static int	draw_roof(int id, t_proj proj, t_color *buf, u_int32_t *ids)
 {
 	const u_int32_t	*verif = (u_int32_t *)buf;
 	int				i;
 
 	i = 0;
+	while (i < proj.top && i < HEIGHT)
+	{
+		if (verif[i * WIDTH + id] == 0)
+			buf[i * WIDTH + id] = get_roof_pixel(proj.plane, proj.tex, i);
+		++i;
+	}
+	return (i);
+}
+
+static void	draw_floor(int id, t_proj proj, t_color *buf, u_int32_t *ids)
+{
+	const u_int32_t	*verif = (u_int32_t *)buf;
+	int i;
+
+	i = (proj.bot >= 0) ? proj.bot : 0;
+	while (i < HEIGHT)
+	{
+		if (verif[i * WIDTH + id] == 0)
+			buf[i * WIDTH + id] = get_floor_pixel(proj.plane, proj.tex, i);
+		++i;
+	}
+}
+
+void		draw_wall(int id, t_proj proj, t_color *buf, u_int32_t *ids)
+{
+	const u_int32_t	*verif = (u_int32_t *)buf;
+	int				i;
+
+	i = draw_roof(id, proj, buf, ids);
 	while (i < proj.top && i < HEIGHT)
 	{
 		if (verif[i * WIDTH + id] == 0)
@@ -30,30 +59,19 @@ void	draw_wall(int id, t_proj proj, t_color *buf, u_int32_t *ids)
 			buf[i * WIDTH + id] = get_wall_pixel(proj, i);
 		++i;
 	}
-	while (i < HEIGHT)
-	{
-		if (verif[i * WIDTH + id] == 0)
-			buf[i * WIDTH + id] = get_floor_pixel(proj.plane, proj.tex, i);
-		++i;
-	}
+	draw_floor(id, proj, buf, ids);
 }
 
-void	draw_portal(int id, t_proj proj, t_color *buf, u_int32_t *ids)
+void		draw_portal(int id, t_proj proj, t_color *buf, u_int32_t *ids)
 {
 	const u_int32_t	*verif = (u_int32_t *)buf;
 	int				i;
 
-	i = 0;
-	while (i < proj.top && i < HEIGHT)
-	{
-		if (verif[i * WIDTH + id] == 0)
-			buf[i * WIDTH + id] = get_roof_pixel(proj.plane, proj.tex, i);
-		++i;
-	}
+	i = draw_roof(id, proj, buf, ids);
 	while (i < proj.ceil && i < HEIGHT)
 	{
 		if (verif[i * WIDTH + id] == 0)
-			buf[i * WIDTH + id] = RED;
+			buf[i * WIDTH + id] = get_wall_pixel(proj, i);
 		++i;
 	}
 	if (proj.step < proj.bot)
@@ -62,16 +80,9 @@ void	draw_portal(int id, t_proj proj, t_color *buf, u_int32_t *ids)
 		while (i < proj.bot && i < HEIGHT)
 		{
 			if (verif[i * WIDTH + id] == 0)
-				buf[i * WIDTH + id] = RED;
+				buf[i * WIDTH + id] = get_wall_pixel(proj, i);
 			++i;
 		}
 	}
-	else
-		i = (proj.bot >= 0) ? proj.bot : 0;
-	while (i < HEIGHT)
-	{
-		if (verif[i * WIDTH + id] == 0)
-			buf[i * WIDTH + id] = get_floor_pixel(proj.plane, proj.tex, i);
-		++i;
-	}
+	draw_floor(id, proj, buf, ids);
 }
