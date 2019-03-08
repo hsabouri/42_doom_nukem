4f8707139c9d25b6ad9e18495594db809662485c
Hugo Sabourin <hsabouri@student.42.fr>
2019-03-08 16:51:24 +0100

    Segfault on checker (unitialized lists)

--- srcs/checker_niveau/convex_sector.c
+++ srcs/checker_niveau/convex_sector.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   convex_sector.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iporsenn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hugo <hugo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 17:43:33 by iporsenn          #+#    #+#             */
-/*   Updated: 2019/02/26 17:43:34 by iporsenn         ###   ########.fr       */
+/*   Updated: 2019/03/08 16:43:57 by hugo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,7 @@ t_list			convex_sector(t_sector *sectors, size_t nsectors, t_game game)
 	t_check_sect	data;
 
 	cpt = 0;
+	error = lnew(NULL);
 	while (cpt < nsectors)
 	{
 		data.first = sectors[cpt].start;
@@ -101,6 +102,7 @@ t_list			parallel_portal(t_portal *portals, size_t nportals, t_game game)
 	t_check_sect	data;
 
 	cpt = -1;
+	error = lnew(NULL);
 	while (++cpt < nportals)
 	{
 		data.ab = vec2_new(game.points[game.walls[portals[cpt].from_wall].b].u -
