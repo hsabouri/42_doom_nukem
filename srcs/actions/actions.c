b8ca6d3c175d10716dbbbad328d8f5baa90d02ea
Leo BOUGEROLLES <lbougero@e2r10p16.42.fr>
2019-05-19 18:07:32 +0200

    animate function created but not implement

--- srcs/actions/actions.c
+++ srcs/actions/actions.c
@@ -6,7 +6,7 @@
 /*   By: lbougero <lbougero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 11:47:39 by hsabouri          #+#    #+#             */
-/*   Updated: 2019/05/18 18:53:29 by lbougero         ###   ########.fr       */
+/*   Updated: 2019/05/19 18:06:54 by lbougero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,15 @@
 
 void        spawn_entity(t_vec2 pos, size_t class_type, t_game game)
 {
-    create_entity(pos, 0, game);
+    // create_entity(pos, 0, game);
     // assign_entity(t_assign_entity *state, t_game game)
-
-    
 }
 
-void        kill_entity(t_entity, t_game game)
+void        kill_entity(int entity_id, t_game * game)
 {
     /*
     int i;
 
-    entity->life = 0;
     i = 0; 
     while(game.entity[i] != entity_id)
     {
@@ -41,26 +38,71 @@ void        animate_sector(t_sector * move_sector)
 {
     /*
         if (is_animate == false)
-            if(M_S.F == A)
-                animate(&move_sector.floor, move_sector.B_pos_floor)
-            if(M_S.C == A)
-                animate(&move_sector.ceiling, move_sector.B_pos_ceiling)
-            if(M_S.F == B)
-                animate(&move_sector.floor, move_sector.A_pos_floor)
-            if(M_S.C == B)
-                animate(&move_sector.ceiling, move_sector.A_pos_ceiling)
+            if(move_sector.floor == move_sector.floor_a))
+                add_animate(&move_sector.floor, move_sector.floor_b)
+            if(move_sector.ceiling == move_sector.ceiling_a)
+                add_animate(&move_sector.ceiling, move_sector.ceiling_b)
+            if(move_sector.floor == move_sector.ceiling_b)
+                add_animate(&move_sector.floor, move_sector.floor_a)
+            if(move_sector.ceiling == move_sector.floor_b)
+                add_animate(&move_sector.ceiling, move_sector.ceiling_a)
         }
     */
 }
 
-void        health_down(int entity_id)
+void        health_down(t_entity * entity, int oof, t_game * game)
 {
-
+    // entity->life -= oof;
+    // if (entity->life <= 0)
+    //      kill_entity(entity->id, game);
 }
 
-void        health_up()
+void        health_up(t_entity * entity, int health)
 {
-    
+    // entity->life += oof;
+    // if (entity->life > 100)
+        // entity->life = 100;
 }
 
+void add_animate(t_vec3 * moveTo, t_vec3 moveGoal, t_game *game)
+{
+ /*
+    t_list_anim * new_element = (t_list_anim *)malloc(sizeof(t_list_anim));
+    *new_element = (t_list_anim) {
+        (t_pelem){NULL,NULL},
+        (t_anim_element){&moveTo,moveGoal}
+        };
+    }
+    if (game->anim.len == 0)
+    {
+        t_plist	anim = lpnew((t_pelem *)new_element);
+        game->anim = anim;
+    } else {
+        lppush(game->anim, (t_pelem *)new_element;
+    }
+ */
+}
 
+void    loading_anim(t_game game) // APPELER TOUTE LES (X A DETERMINER) SECONDES
+{
+    // t_list_anim    *current;
+    // while ((current = (t_list_anim *)lpnext(&game.anim)) != NULL)
+    // {
+    //     current->a_elem.moveTo = move_anim(current);
+    //     if (current->a_elem.moveTo == current->a_elem.moveGoal)
+    //         lpremove(&game.anim, (t_pelem *)current);
+    // }   
+}
+
+t_vec3      move_anim(t_list_anim c_anim)
+{
+    // if (c_anim.a_elem.moveTo < c_anim.a_elem.moveGoal)
+    // {
+    //     c_anim.a_elem.moveTo += 0.1;
+    // }
+    // else if (c_anim.a_elem.moveTo > c_anim.a_elem.moveGoal)
+    // {
+    //     c_anim.a_elem.moveTo -= 0.1;
+    // }
+    // return (c_anim.a_elem.moveTo);
+}
