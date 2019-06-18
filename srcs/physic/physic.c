5d71069dc860ee1e0210f5bc596ee9d24c8a538f
Isabelle PORSENNA <iporsenn@e2r10p13.42.fr>
2019-06-18 17:45:04 +0200

    no physic on button

--- srcs/physic/physic.c
+++ srcs/physic/physic.c
@@ -92,13 +92,16 @@ t_game			physic(t_game game, t_event events, float old_timer)
 	new_game = game;
 	while (i < game.nentities)
 	{
-		new_physic = game.entities[i].physic;
-		if (game.entities[i].damage >= 1)
-			new_physic.speed = set_entity_speed(new_physic,
-				game.player.my_entity.physic, old_timer);
-		new_physic = entities_look_at(new_physic, game.player.my_entity.physic);
-		new_game.entities[i].physic = entities_physic(new_physic, &game, i,
-		old_timer);
+		if (game.entities[i].type < 14 || game.entities[i].type > 21)
+		{
+			new_physic = game.entities[i].physic;
+			if (game.entities[i].damage >= 1)
+				new_physic.speed = set_entity_speed(new_physic,
+					game.player.my_entity.physic, old_timer);
+			new_physic = entities_look_at(new_physic, game.player.my_entity.physic);
+			new_game.entities[i].physic = entities_physic(new_physic, &game, i,
+			old_timer);
+		}
 		i++;
 	}
 	new_physic = game.player.my_entity.physic;
